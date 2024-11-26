1fa2ceefff28d4a39d86d5fbf595bfb4d5ff01eb
Michael <michael.njoroge3@student.moringaschool.com>
2024-11-26 17:02:41 +0300

    add all commit

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
+const num1 = 62;
+const num2 = 1;
+const multiply = num1 * num2;
 
-// Write your code here
+
+const random = Math.floor( Math.random() * 1000 ) + 1;
+
+
+const num3 = 36;
+const num4 = 8;
+const mod = num3 % num4;
+
+
+const max = Math.max( 4, 11, 17, 20 );
+
+console.log(Number)
